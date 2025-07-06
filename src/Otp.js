06eff7c7faa6f22783d2bc254ed2c064b538c97a
richakalaria <richakalaria@gmail.com>
2025-07-06 19:41:23 +0530

    Inital commit

---
+++ src/Otp.js
@@ -0,0 +1,13 @@
+function Otp() {
+  return (
+    <div className="Otp">
+      <header className="Otp-header">
+        <p>
+          OTP Code 123
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Otp;
